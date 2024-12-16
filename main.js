bf32d6547d7f0a69e9dc63124907007b3ff97154
TheSquiddyLink <preston@happychaps.com>
2024-12-16 11:08:32 -0700

    Move Get Playlist to own function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import http from 'http';
+import http, { get } from 'http';
 import fs from 'fs';
 import path from 'path';
 import formidable from 'formidable'
@@ -9,24 +9,7 @@ const server = http.createServer((req, res) => {
     const filePath = path.join(folder, 'index.html');
 
     if(req.url == "/getPlaylist"){
-        const form = formidable({});
-        form.parse(req, (err, fields, files) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Server Error');
-                return
-            }
-            const playlist = fields.playlist[0];
-                let fileData;
-                try {
-                    fileData = fs.readFileSync(playlist, 'utf8');
-                } catch (err) {
-                    res.writeHead(404, { 'Content-Type': 'text/plain' });
-                    res.end('File Not Found');
-                    return;
-                }            
-                console.log(fileData);
-        })
+        getPlaylist(req, res);
         return;
     }
     fs.readFile(filePath, (err, data) => {
@@ -44,3 +27,29 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+/**
+ * 
+ * @param {http.IncomingMessage} req 
+ * @param {http.ServerResponse<http.IncomingMessage>} res 
+ */
+function getPlaylist(req, res){
+    const form = formidable({});
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Server Error');
+            return
+        }
+        const playlist = fields.playlist[0];
+            let fileData;
+            try {
+                fileData = fs.readFileSync(playlist, 'utf8');
+            } catch (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('File Not Found');
+                return;
+            }            
+            console.log(fileData);
+    })
+}
